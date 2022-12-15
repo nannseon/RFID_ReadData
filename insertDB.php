7dec93a0fd7d0698c8c38cf66986e0fbb74ab516
Nan <nantikapat.2000@gmail.com>
2022-12-15 16:07:41 +0700

    first commit

---
+++ insertDB.php
@@ -0,0 +1,20 @@
+<?php
+     
+    require 'database.php';
+ 
+    if ( !empty($_POST)) {
+        // keep track post values
+        $name = $_POST['name'];
+		$id = $_POST['id'];
+	
+        
+		// insert data
+        $pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "INSERT INTO table_the_iot_projects (name,id) values(?, ?)";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($name,$id));
+		Database::disconnect();
+		header("Location: user data.php");
+    }
+?>
